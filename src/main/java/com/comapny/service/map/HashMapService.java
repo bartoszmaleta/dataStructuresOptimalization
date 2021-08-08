3fbd7d858981ab98e7a7ac94530267b9e69c944e
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-08-08 19:39:42 +0200

    [FIX] method access modifiers

--- src/main/java/com/comapny/service/map/HashMapService.java
+++ src/main/java/com/comapny/service/map/HashMapService.java
@@ -1,13 +1,14 @@
 package com.comapny.service.map;
 
 public class HashMapService {
-    private static final java.util.Map<Integer, String> myMap = new java.util.HashMap<>();
+
+    public static final java.util.Map<Integer, String> myMap = new java.util.HashMap<>();
     static {
         myMap.put(1, "one");
         myMap.put(2, "two");
     }
 
-    private static final java.util.Map<Integer, String> myJava9Map = java.util.Map.of(
+    public static final java.util.Map<Integer, String> myJava9Map = java.util.Map.of(
             1, "one",
             2, "two"
     );
