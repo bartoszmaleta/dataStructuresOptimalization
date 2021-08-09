5f937c88fbb179ca6ce1618af371806212ef5c71
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-08-09 22:43:40 +0200

    [ADD] iterate and join examples

--- src/main/java/com/comapny/service/list/UtilListService.java
+++ src/main/java/com/comapny/service/list/UtilListService.java
@@ -1,4 +1,28 @@
 package com.comapny.service.list;
 
+import java.util.stream.Collectors;
+
 public class UtilListService {
+
+    // Iterate
+    public java.util.List<Integer> legacyIterate(Integer... ints) {
+        return java.util.Arrays
+                .stream(ints)
+                .map(i -> i + 1)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    }
+
+    // Join
+    public String legacyJoin(String... args) {
+        return java.util.Arrays
+                .stream(args)
+                .collect(Collectors.joining(", "));
+    }
+
+    public String stringJavaJoiner(String... args) {
+        return String.join(", ", args);
+    }
+
+
 }
--- src/main/java/com/comapny/service/vavr/VavrListService.java
+++ src/main/java/com/comapny/service/vavr/VavrListService.java
@@ -1,4 +1,20 @@
 package com.comapny.service.vavr;
 
 public class VavrListService {
+
+    // Iterate
+    public io.vavr.collection.Seq<Integer> vavrIterate(Integer... ints) {
+        return io.vavr.collection.Stream
+                .of(ints)
+                .map(i -> i + 1)
+                .peek(System.out::println);
+    }
+
+    // Join
+    public String vavrJoin(String... args) {
+        return io.vavr.collection.List
+                .of(args)
+                .mkString(", ");
+    }
+
 }
